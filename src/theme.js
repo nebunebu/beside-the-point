4dd52cd5e07b3b848f6f2b0ab388d45f34ca900e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-12-01 20:34:39 -0500

    add theme.js

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+export const rosePineTheme = {
+  light: {  // Dawn
+    base: '#faf4ed',
+    surface: '#fffaf3',
+    text: '#575279',
+    subtle: '#6e6a86',
+    muted: '#9893a5',
+    iris: '#907aa9',
+    pine: '#286983',
+    rose: '#d7827e',
+    love: '#b4637a',
+    gold: '#ea9d34',
+    border: '#dfdad9',
+  },
+  dark: {
+    base: '#191724',
+    surface: '#1f1d2e',
+    text: '#e0def4',
+    subtle: '#908caa',
+    muted: '#6e6a86',
+    iris: '#c4a7e7',
+    pine: '#31748f',
+    rose: '#ebbcba',
+    love: '#eb6f92',
+    gold: '#f6c177',
+    border: '#26233a',
+  }
+};
